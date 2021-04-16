65150e69c92028f6bccadf977f32fd9e33b62a03
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-04-16 19:17:20 -0300

    Agrega archivos base de proyecto

---
+++ backend/src/main/kotlin/ar/edu/unq/readtogether/readtogether/ReadtogetherApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.readtogether.readtogether
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReadtogetherApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReadtogetherApplication>(*args)
+}
---
+++ backend/src/test/kotlin/ar/edu/unq/readtogether/readtogether/ReadtogetherApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.readtogether.readtogether
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReadtogetherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
