69752d647d6898bcd9062ad3ce33fb010be4d249
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-04-28 20:51:04 -0300

    WIP

--- backend/src/main/kotlin/ar/edu/unq/readtogether/readtogether/controllers/GruposController.kt
+++ backend/src/main/kotlin/ar/edu/unq/readtogether/readtogether/controllers/GruposController.kt
@@ -1,13 +1,18 @@
 package ar.edu.unq.readtogether.readtogether.controllers
 
+import ar.edu.unq.readtogether.readtogether.repositories.GrupoRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class GruposController {
 
+    @Autowired
+    private lateinit var grupoRepository: GrupoRepository
+
     @GetMapping("/grupos")
     fun buscarGrupos(): List<String> {
-        return listOf<String>("El señor de los anillos")
+        return grupoRepository.obtenerGrupos()
     }
 }
--- backend/src/main/kotlin/ar/edu/unq/readtogether/readtogether/repositories/GrupoRepository.kt
+++ backend/src/main/kotlin/ar/edu/unq/readtogether/readtogether/repositories/GrupoRepository.kt
@@ -3,12 +3,16 @@ package ar.edu.unq.readtogether.readtogether.repositories
 import org.springframework.stereotype.Repository
 
 @Repository
-class GrupoRepository : CRUDRepository{
+class GrupoRepository{
 
     var library = mutableListOf<String>()
 
     fun save(grupo: String) {
         library.add(grupo)
+    }
+
+    fun obtenerGrupos(): List<String> {
+        return library
     }
 
 }
