f86f4480bc8a0a76582df6aefbbb62d807bce933
Juan Staffora <juanstaffora@gmail.com>
2021-04-28 09:23:55 -0300

    comiteo la busqueda

---
+++ backend/src/main/kotlin/ar/edu/unq/readtogether/readtogether/controllers/GruposController.kt
@@ -0,0 +1,14 @@
+package ar.edu.unq.readtogether.readtogether.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GruposController {
+
+    @GetMapping("/grupos")
+    fun buscarGrupos() {
+
+    }
+
+}
