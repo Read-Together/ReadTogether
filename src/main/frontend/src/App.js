4d8ae0db605f460d65d70fe267787510b8093cc6
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-05-05 09:29:32 -0300

    Extrae barra de navegación a /components

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, useParams} from 'react-router-dom'
-import SearchForm from "./components/SearchForm";
 import {useState} from "react";
+import NavBar from "./components/NavBar"
 
 const axios = require('axios').default;
 
@@ -28,46 +28,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#"><h1 className="animated flash">Read Together</h1></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"/>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto me-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                     data-bs-toggle="dropdown"
-                     aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                      <hr className="dropdown-divider"/>
-                    </li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-                </li>
-              </ul>
-              <SearchForm/>
-            </div>
-          </div>
-        </nav>
+        <NavBar/>
       </div>
       <Switch>
         <Route path="/busqueda/:termino" component={Resultados}>
