2442f6b48cc9d9c33b251571e0036ea3e3541553
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-05-07 08:55:39 -0300

    Envía información de creacion de grupo al backend

--- src/main/frontend/src/components/FormularioCrearComunidad.js
+++ src/main/frontend/src/components/FormularioCrearComunidad.js
@@ -1,16 +1,43 @@
-import {Button, Form} from "react-bootstrap";
+import {Alert, Button, Form} from "react-bootstrap";
+import {useState} from "react";
+import axios from "axios";
 
 export function FormularioCrearComunidad() {
+  const [nombre, setNombre] = useState("")
+  const [descripcion, setdescripcion] = useState("")
+  const [fueCreado, setFueCreado] = useState(false)
+  const [noFueCreado, setNoFueCreado] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    
+    axios.post("http://localhost:8080/grupos", {
+      nombre: "nombre",
+      descripcion: "descripcion"
+    })
+      .then(() => {
+        setFueCreado(true)
+      })
+      .catch(() => {
+        setNoFueCreado(true)
+      });
+    
+  } ;
+  
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
+      <Alert variant="success" show={fueCreado}>¡Genial! Has creado el grupo {nombre} </Alert>
+      <Alert variant="danger" show={noFueCreado}>¡Ups! Ocurrió un error creando el grupo {nombre}, intenta mas tarde </Alert>
       <Form.Group controlId="nombre">
         <Form.Label>Nombre de la comunidad</Form.Label>
-        <Form.Control type="text" placeholder="Nombre"/>
+        <Form.Control type="text" placeholder="Nombre" value={nombre}
+                      onChange={event => setNombre(event.target.value)}/>
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Descripción</Form.Label>
-        <Form.Control as="textarea" rows={3} placeholder="Cuenta de que se trata"/>
+        <Form.Control as="textarea" rows={3} placeholder="Cuenta a la gente de que se trata" value={descripcion}
+                      onChange={event => setdescripcion(event.target.value)}/>
       </Form.Group>
 
       <Button variant="primary" type="submit">
