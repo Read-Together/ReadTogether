78a629fca2d5410c9b159ae72532999c0f81ae20
Gonzalo <gonzalodiaz1994@gmail.com>
2021-05-10 00:08:42 -0300

    Frontend del registro de usuario ahora está conectado con el backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import {BrowserRouter as Router, Route, Switch, useParams} from 'react-router-do
 import SearchForm from "./components/SearchForm";
 import {useState} from "react";
 import Register from './components/Register';
-import RegisterForm from './components/RegisterForm';
+import "./css/Register.css"
+import { Link } from 'react-router-dom';
 
 const axios = require('axios').default;
 
@@ -34,48 +35,24 @@ function App() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="#"><h1 className="animated flash">Read Together</h1></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"/>
-            </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto me-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                     data-bs-toggle="dropdown"
-                     aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                      <hr className="dropdown-divider"/>
-                    </li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                  <Link to= "/">
+                  <a className="nav-link active" aria-current="page">Home</a>
+                  </Link>
                 </li>
               </ul>
-            
-                <Register/>
+              <Link to="/registrar" class="espaciado">
+              <button type="button" class="btn btn-primary"> Registrarme</button>
+              </Link>
                 <SearchForm/>
             </div>
           </div>
         </nav>
       </div>
       <Switch>
-        <Route path="/registro/" component={RegisterForm}></Route>
+        <Route path="/registrar" component={Register}></Route>
         <Route path="/busqueda/:termino" component={Resultados} ></Route>
         <Route path="/">
 
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,33 +1,105 @@
-import {Component} from "react";
-import {withRouter} from "react-router-dom";
+import axios from "axios";
+import { useHistory} from "react-router-dom";
+import React, { useState } from "react";
+import "../css/Register.css";
+import {Alert} from "react-bootstrap";
 
-import React from 'react';
+const Register = () => {
+  const history = useHistory();
+  const[registrado, setRegistrado] = useState(false);
+  const[noRegistrado, setNoRegistrado] = useState(false);
+  const [data, setData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
 
-class Register extends Component {
+  const handleInputChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-    this.cambiarInput = this.cambiarInput.bind(this);
-  }
+  const handleSubmit = (event) => {
+    setNoRegistrado(false)
+    setRegistrado(false)
+    event.preventDefault();
+    axios
+      .post('http://localhost:8080/registrar', data)
+      .then( () =>{
+        setRegistrado(true)
+        setTimeout(() => {
+          history.push("/")
+        }, 3000)
+      })
+      .catch((error) => {
+        setRegistrado(false)
+        setNoRegistrado(true)
+      });
+      
+  };
 
-  render() {
-
-    return (
-      <form Class="d-flex" onSubmit={event => {
-        event.preventDefault()
-        this.props.history.replace(`registro/`)
-      }}>
-        <button Class="btn btn-outline-success">Registrarme</button>
+  return (
+    <div class="cardRegistrate">
+      <form id="Registro" onSubmit={handleSubmit} class="row g-3">
+      <Alert variant="danger" show={noRegistrado}>¡Ups! Ocurrió un error </Alert>
+      <Alert variant="success" show={registrado}>¡Genial! Te has registrado {data.userName} </Alert>
+      
+        <div>
+          <label for="inputCity" class="form-label">
+            Nombre de Usuario
+          </label>
+          <input
+            type="text"
+            name="userName"
+            value={data.username}
+            placeholder="Nombre de usuario"
+            onChange={handleInputChange}
+            class="form-control"
+            id="inputCity"
+          />
+        </div>
+        <div>
+          <label for="inputEmail4" class="form-label">
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            class="form-control"
+            id="inputEmail4"
+            value={data.email}
+            placeholder="Correo electrónico"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label for="inputPassword4" class="form-label">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            name="password"
+            class="form-control"
+            id="inputPassword4"
+            value={data.password}
+            placeholder="Contraseña"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <button type="submit" 
+          disabled={
+            !(data.email && data.password && data.userName)
+          }
+          class="btn btn-primary">
+            Registrar
+          </button>
+        </div>
       </form>
-    )
-  }
+    </div>
+  );
+};
 
-  cambiarInput(event) {
-    this.setState({value: event.target.value})
-  }
-
-}
-
-  export default withRouter(Register);
-  
+export default Register;
