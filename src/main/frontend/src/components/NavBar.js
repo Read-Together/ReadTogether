3e1c0c549ce84ed20a3a08ee1e83527dcac1c76f
Gonzalo <gonzalodiaz1994@gmail.com>
2021-05-19 03:07:01 -0300

    Cerrar sesión terminado.

--- src/main/frontend/src/components/NavBar.js
+++ src/main/frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "../css/NavBar.css";
-import { useState} from "react";
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 
 const NavBar = () => {
@@ -12,35 +12,34 @@ const NavBar = () => {
   };
 
 
+  const getNombreUsuario = () => {
+    return sessionStorage.getItem("loggedUsername");
+  };
+
+  const cerrarSesion = () => {
+    sessionStorage.removeItem("accessToken");
+    sessionStorage.removeItem("loggedUsername");
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="/home">
           <h1 className="animated flash">Read Together</h1>
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
           <ul className="navbar-nav me-auto me-2 mb-lg-0">
-
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/home">
-                {sessionStorage.getItem("loggedUsername")}
+                {getNombreUsuario()}
               </a>
             </li>
           </ul>
-
-          <Button className="btn btn-danger cerrarSesion">
+          <Button
+            className="btn btn-danger cerrarSesion"
+            onClick={() => cerrarSesion()}
+            href="/home"
+          >
             Cerrar sesión
           </Button>
 
