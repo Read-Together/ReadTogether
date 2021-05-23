337ab9991c92249724a335a5111c4bf04c823246
Gonzalo <gonzalodiaz1994@gmail.com>
2021-05-23 20:27:44 -0300

    Agregado test que comprueba que se creó un token al iniciar sesion

--- src/test/kotlin/ar/edu/unq/readtogether/readtogether/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unq/readtogether/readtogether/UsuarioTest.kt
@@ -11,10 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+
 
 
 @SpringBootTest
@@ -27,16 +27,16 @@ class UsuarioTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
     @Autowired
-    private lateinit var service: UsuarioService
+    private lateinit var usuarioService: UsuarioService
 
     @BeforeAll
     fun setUp(){
-        service.eliminarDatos()
+        usuarioService.eliminarDatos()
     }
 
     @AfterEach
     fun clean(){
-        service.eliminarDatos()
+        usuarioService.eliminarDatos()
     }
 
     @Test
@@ -45,7 +45,7 @@ class UsuarioTest {
         mockMvc.perform(MockMvcRequestBuilders.post("/registrar")
                 .content(ObjectMapper().writeValueAsString(usuario))
                 .contentType(MediaType.APPLICATION_JSON))
-        var user = service.buscarUsuario(usuario)
+        var user = usuarioService.buscarUsuario(usuario)
         assert(user!!.userName == usuario.userName)
     }
 
@@ -61,7 +61,7 @@ class UsuarioTest {
     @Test
     fun cuandoAgregoUnUsuarioQueYaExiste_retornaUn400(){
         usuario = Usuario("juan","juan@gmail.com","1234")
-        service.registrarUsuario(usuario)
+        usuarioService.registrarUsuario(usuario)
         mockMvc.perform(MockMvcRequestBuilders.post("/registrar")
                 .content(ObjectMapper().writeValueAsString(usuario))
                 .contentType(MediaType.APPLICATION_JSON))
@@ -71,7 +71,7 @@ class UsuarioTest {
     @Test
     fun cuandoInicioSesion_retornaUn200(){
         usuario = Usuario("juan2","juan2@gmail.com","1234")
-        service.registrarUsuario(usuario)
+        usuarioService.registrarUsuario(usuario)
         var request = RequestUsuario("juan2", "1234")
         mockMvc.perform(MockMvcRequestBuilders.post("/login")
                 .content(ObjectMapper().writeValueAsString(request))
@@ -80,9 +80,21 @@ class UsuarioTest {
     }
 
     @Test
+    fun cuandoInicioSesion_MeDevuelveUnToken(){
+        usuario = Usuario("barbi","barbi@gmail.com","1234")
+        usuarioService.registrarUsuario(usuario)
+        var request = RequestUsuario("barbi", "1234")
+        var response = mockMvc.perform(MockMvcRequestBuilders.post("/login")
+            .content(ObjectMapper().writeValueAsString(request))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andReturn().response.contentAsString
+        assert(response.contains("Bearer "))
+    }
+
+    @Test
     fun cuandoInicioSesionConDatosErroneos_arrojaComoExceptionUsuarioYOContraseniaInvalidos(){
         usuario = Usuario("juan3","juan3@gmail.com","1234")
-        service.registrarUsuario(usuario)
+        usuarioService.registrarUsuario(usuario)
         var usuarioLogin = RequestUsuario("juan3", "123")
         assertThatThrownBy {
             mockMvc.perform(MockMvcRequestBuilders.post("/login")
