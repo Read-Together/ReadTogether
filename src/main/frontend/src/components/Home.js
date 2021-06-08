c67cf6397eeb662c4a26bb2b1340c9525e2d9436
Gonzalo <gonzalodiaz1994@gmail.com>
2021-06-08 02:28:16 -0300

    WIP Frontend, agregado el llamado a la API

--- src/main/frontend/src/components/Home.js
+++ src/main/frontend/src/components/Home.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NavBar from "./NavBar";
 import {LeftMenu} from "./LeftMenu";
+import axios from "axios";
+import { useParams } from "react-router";
 
 const Home = () => {
+  const[data, setData] = useState([]);
+
+  useEffect(() => {
+    getComunidades();
+  }, []);
+
+  const getComunidades = () => {
+    const header = {
+      authorization: sessionStorage.getItem("accessToken"),
+    };
+
+    const usuario = sessionStorage.getItem("loggedUsername")
+
+    axios
+      .get(`http://localhost:8080/home/${usuario}`, {headers: header})
+      .then((response) => {
+        setData(response.data)
+        console.log(data);
+      });
+  }
 
     return (
         <div className="App">
@@ -10,6 +32,7 @@ const Home = () => {
           <div className="body-container">
             <LeftMenu/>
             <div className="main-content">
+              
             </div>
           </div>
         </div>
