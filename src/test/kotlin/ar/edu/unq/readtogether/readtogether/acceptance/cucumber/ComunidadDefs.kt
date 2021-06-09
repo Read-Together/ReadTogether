bac7c7559aa759633b1334340d0862f3d0a31108
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-06-09 19:49:38 -0300

    Agrega tests de unirse y salir de una comunidad

--- src/test/kotlin/ar/edu/unq/readtogether/readtogether/acceptance/cucumber/ComunidadDefs.kt
+++ src/test/kotlin/ar/edu/unq/readtogether/readtogether/acceptance/cucumber/ComunidadDefs.kt
@@ -3,41 +3,71 @@ package ar.edu.unq.readtogether.readtogether.acceptance.cucumber
 import ar.edu.unq.readtogether.readtogether.dtos.CreacionDeGruposForm
 import ar.edu.unq.readtogether.readtogether.services.GrupoService
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
 import org.assertj.core.api.Assertions.assertThat
+import org.json.JSONObject
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 
 @AutoConfigureMockMvc
 class ComunidadDefs : SpringIntegrationTest() {
 
     private val nombreGrupo = "Fans de Tolkien"
     private val descripcionGrupo = "Un grupo de hobbits"
-    private lateinit var idDelGrupo : String
+    private lateinit var idDelGrupo: String
 
     @Autowired
     private lateinit var mockMvc: MockMvc
+
     @Autowired
     private lateinit var grupoService: GrupoService
+
     @Autowired
     private lateinit var context: StepDefinitionsContext
 
+    @Given("una comunidad ya creada de nombre \"([^\"]*)\"$")
+    fun crearComunidadDeNombre(nombre: String) {
+        idDelGrupo = grupoService.crearGrupo(nombre, "una descripcion")
+    }
+
     @When("crea una comunidad")
-    fun crearUnaComunidad(){
+    fun crearUnaComunidad() {
         val creacionDeGruposForm = CreacionDeGruposForm(nombreGrupo, descripcionGrupo)
         context.perform(
-                MockMvcRequestBuilders.post("/grupos")
+                post("/grupos")
                         .header("Authorization", context.token)
                         .content(ObjectMapper().writeValueAsString(creacionDeGruposForm))
                         .contentType(MediaType.APPLICATION_JSON), mockMvc)
     }
 
+    @Given("el usuario pertenece a la comunidad \"([^\"]*)\"$")
+    @When("el usuario se une a la comunidad \"([^\"]*)\"$")
+    fun usuarioSeUneAComunidad(nombreDeComunidad: String) {
+        val id = grupoService.obtenerGruposConNombre(nombreDeComunidad).first().id
+        context.perform(post("/grupos/${id}/registrar")
+                .header("Authorization", context.token)
+                .content(JSONObject().put("userName", username).toString())
+                .contentType(MediaType.APPLICATION_JSON), mockMvc
+        )
+    }
+
+    @When("el usuario sale de la comunidad \"([^\"]*)\"$")
+    fun sacarUsuarioDeComunidad(nombreDeComunidad: String) {
+        val id = grupoService.obtenerGruposConNombre(nombreDeComunidad).first().id
+        context.perform(post("/grupos/${id}/salir")
+                .header("Authorization", context.token)
+                .content(JSONObject().put("userName", username).toString())
+                .contentType(MediaType.APPLICATION_JSON), mockMvc
+        )
+    }
+
     @Then("obtiene el id de la comunidad creada")
-    fun assertarQueObtieneIdDeUnaComunidad(){
+    fun assertarQueObtieneIdDeUnaComunidad() {
         idDelGrupo = context.andReturn().response.contentAsString
         val grupoConId = grupoService.obtenerGrupoDeID(idDelGrupo)
         assertThat(grupoConId.nombre).isEqualTo(nombreGrupo)
@@ -45,9 +75,15 @@ class ComunidadDefs : SpringIntegrationTest() {
     }
 
     @Then("es miembro del grupo")
-    fun assertarQueElUsuarioEsMiembroDelGrupo(){
+    fun assertarQueElUsuarioEsMiembroDelGrupo() {
         val grupoConId = grupoService.obtenerGrupoDeID(idDelGrupo)
         assertThat(grupoConId.usuarios).contains(username)
+    }
+
+    @Then("el usuario ya no pertenece a la comunidad \"([^\"]*)\"$")
+    fun assertarQueElUsuarioNoEsMiembroDelGrupo(nombreDeComunidad: String) {
+        val grupo = grupoService.obtenerGruposConNombre(nombreDeComunidad).first()
+        assertThat(grupo.usuarios).doesNotContain(username)
     }
 
 }
--- src/test/resources/cucumber/comunidad.feature
+++ src/test/resources/cucumber/comunidad.feature
@@ -1,6 +1,19 @@
 Feature: Comunidades
+
   Scenario: Usuario crea una comunidad
     Given un usuario logeado
     When crea una comunidad
     Then obtiene el id de la comunidad creada
-    And es miembro del grupo
+
+  Scenario: Usuario se une a una comunidad
+    Given un usuario logeado
+    And una comunidad ya creada de nombre "los capos"
+    When el usuario se une a la comunidad "los capos"
+    Then es miembro del grupo
+
+  Scenario: Usuario sale de una comunidad
+    Given un usuario logeado
+    And una comunidad ya creada de nombre "los capos"
+    And el usuario pertenece a la comunidad "los capos"
+    When el usuario sale de la comunidad "los capos"
+    Then el usuario ya no pertenece a la comunidad "los capos"
